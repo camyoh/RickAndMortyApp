8fc5aef7af15d0ca32e5506e9adcedf0d16d6f32
Andres <carlos.camyoh@gmail.com>
2023-06-24 17:29:17 -0500

    Initial Commit

---
+++ Rick And Morty/Rick_And_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_And_MortyApp.swift
+//  Rick And Morty
+//
+//  Created by Andres Mendieta on 24/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Rick_And_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
